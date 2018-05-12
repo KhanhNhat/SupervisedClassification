c7c45bd5c0de9490582b83ae03d5b1976134ddc9
Nhat Hoang <hoangphamkhanhnhat@gmail.com>
2018-05-13 08:10:14 +1000

    Add a big dataset for testing knn.

--- RScript.R
+++ RScript.R
@@ -3,7 +3,13 @@ library(tidyverse)
 
 trafficSign = read_csv('knn_traffic_signs.csv')
 nextSign = read_csv('nextSign.csv')
-
+testSign = read_csv('TestSign.csv')
 sign_types = trafficSign$sign_type
 
-knn(train = trafficSign[-1], test = nextSign, cl = sign_types)
+#Predict type of the nextSign dataset
+knn(train = trafficSign[, 4:51], test = nextSign, cl = sign_types)
+
+table(sign_types)
+
+#Check variable average by sign_type, for example, r10:
+aggregate(r10 ~ sign_type, data = trafficSign, mean)
